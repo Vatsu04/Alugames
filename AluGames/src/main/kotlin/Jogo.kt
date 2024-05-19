6b3cd3341306cf2a17c82f97b45d3ed8a1ea8322
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-05-19 16:52:04 -0300

    Update

--- AluGames/src/main/kotlin/Jogo.kt
+++ AluGames/src/main/kotlin/Jogo.kt
@@ -1,9 +1,9 @@
 data class Jogo (val titulo:String,
          val capa:String) {
 
-    val descricao = ""
+    val descricao:String? = null
     override fun toString(): String {
-        return "Meu Jogo':\n" +
+        return "Meu Jogo:\n" +
                 "Titulo=  '$titulo' \n" +
                 "Capa='$capa' \n" +
                 "Descricao='$descricao' "
