aa3c445504ae8d16a506668891cae25b644db35b
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2024-05-21 14:31:52 -0300

    Email Invalido

--- AluGames/src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
+++ AluGames/src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -6,11 +6,18 @@ fun main(){
 
     val gamer2 = Gamer("Suares", "suarestk@gmail.com", "20/12/2001, ","Suarestk")
 
+
+    println(gamer1)
+
     gamer1.let{
         it.dataNascimento = "01/04/2004"
         it.usuario = "Vatsu"
-        it.idInterno = "Vatsu1234"
+
+    }.also{
+        println(gamer1.idInterno)
     }
 
+    println(gamer1)
+    gamer1.usuario = "vatsu"
     println(gamer1)
 }
